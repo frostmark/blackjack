208b62530f14b6b9ee9bcc21323eb6d3ffe3b2a6
Mark Frost <cheerful.mf@gmail.com>
2017-04-25 01:08:25 +0500

    game logic

--- lib/blackjack/game.rb
+++ lib/blackjack/game.rb
@@ -1,19 +1,55 @@
 module Blackjack
   class Game
-    attr_reader :player
+    attr_reader :player, :dealer, :bank
 
     def initialize
-      print 'Введите ваше имя: '
-      player_name = gets.chomp
-
-      @player = Player.new(player_name)
-      @deck = Deck.new
+      create_deck
+      create_players
+      create_bank
     end
 
     def run
       loop do
+        start_round
+        puts "In bank: $#{bank.money}"
+        loop do
+          print 'Your choice: '
 
+        end
       end
+    end
+
+    private
+
+    def create_deck
+      @deck = Deck.new
+    end
+
+    def create_players
+      print 'Введите ваше имя: '
+      player_name = gets.chomp
+
+      @player = Player.new(player_name)
+      @dealer = Dealer.new('Dealer')
+
+      2.times do
+        @player.cards << @deck.hand_out
+        @dealer.cards << @deck.hand_out
+      end
+    end
+
+    def start_round
+      debugger
+      bank.get_money(player)
+      bank.get_money(dealer)
+    end
+
+    def create_bank
+      @bank = Bank.new
+    end
+
+    def player_choice
+
     end
   end
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require('byebug')
 require_relative('lib/blackjack')
 
 game = Blackjack::Game.new
+game.run
