7fc554e907249054012969f8894819fd50fb5149
Mark Frost <cheerful.mf@gmail.com>
2017-04-25 01:07:39 +0500

    minor changes for Card class

--- lib/blackjack/card.rb
+++ lib/blackjack/card.rb
@@ -6,5 +6,14 @@ module Blackjack
       @suit = suit
       @rank = rank
     end
+
+    def to_s
+      "#{suit}#{rank}"
+    end
+
+    def point
+      return rank.to_i if ('2'..'10').include?(rank)
+      10 if %w[J Q K].include?(rank)
+    end
   end
 end
